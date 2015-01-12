5cf6a2704775cdaadc7bb459558f3573b159cce8
Chris Hiestand <chiestand@salk.edu>
2015-01-12 00:20:04 -0800

    add -Djava.net.preferIPv4Stack=true to java params
    Seeing if this will fix failure of automatic gce startup by
     using google container manifest metadata file

--- launch.sh
+++ launch.sh
@@ -4,5 +4,6 @@ NEO4J_HOME=/var/lib/neo4j
 
 sed -i "s|#org.neo4j.server.webserver.address=0.0.0.0|org.neo4j.server.webserver.address=$HOSTNAME|g" $NEO4J_HOME/conf/neo4j-server.properties
 sed -i "s|=INFO|=FINEST|g" $NEO4J_HOME/conf/logging.properties
+echo 'wrapper.java.additional=-Djava.net.preferIPv4Stack=true' >>  $NEO4J_HOME/conf/neo4j-wrapper.conf
 ulimit -n 65536 ; .$NEO4J_HOME/bin/neo4j console
 
